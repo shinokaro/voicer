637f9099bf6a5b15b91b4cd5952bf720b7d1d71b
shinokaro <shinokaro@hotmail.co.jp>
2024-08-20 13:30:37 +0900

    Initial commit

---
+++ lib/voicer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "voicer/version"
+
+module Voicer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_voicer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestVoicer < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Voicer::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
